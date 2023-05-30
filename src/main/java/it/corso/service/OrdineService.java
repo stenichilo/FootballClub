1f17ffd5ab6f110dc65372180b3e69aafbcb68a0
stenichilo <stefano.nichilo2@gmail.com>
2023-05-30 20:15:35 +0200

    modifiche 30052023-2015

--- src/main/java/it/corso/service/OrdineService.java
+++ src/main/java/it/corso/service/OrdineService.java
@@ -3,7 +3,6 @@ package it.corso.service;
 import java.util.List;
 
 import it.corso.model.Ordine;
-import it.corso.model.Utente;
 import jakarta.servlet.http.HttpSession;
 
 public interface OrdineService {
