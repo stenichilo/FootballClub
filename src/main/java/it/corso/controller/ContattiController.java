d8d86d997c3affe1d18f7f279f8944c010ebf7af
stenichilo <stefano.nichilo2@gmail.com>
2023-05-30 19:26:52 +0200

    modifiche contatti

--- src/main/java/it/corso/controller/ContattiController.java
+++ src/main/java/it/corso/controller/ContattiController.java
@@ -1,15 +1,20 @@
 package it.corso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/contatti")
 public class ContattiController {
 
 	@GetMapping
-	public String getPage() {
+	public String getPage(HttpSession session, Model model) {
+		model.addAttribute("utente", session.getAttribute("utente") != null);
+		model.addAttribute("admin", session.getAttribute("admin") != null);
 		return "contatti";
 	}
 }
