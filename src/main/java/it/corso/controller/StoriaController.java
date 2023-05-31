70fbd47d742b479d6886a56118484886f8cf9cbe
DaniLestrange <daniela.mangeruca@gmail.com>
2023-05-31 14:56:29 +0200

    modifica storia
    modificato controller e html

--- src/main/java/it/corso/controller/StoriaController.java
+++ src/main/java/it/corso/controller/StoriaController.java
@@ -1,15 +1,20 @@
 package it.corso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/storia")
 public class StoriaController {
 
 	@GetMapping
-	public String getPage() {
+	public String getPage(Model model,HttpSession session) {
+		model.addAttribute("utente", session.getAttribute("utente") != null);
+		model.addAttribute("admin", session.getAttribute("admin") != null);
 		return "storia";
 	}
 }
